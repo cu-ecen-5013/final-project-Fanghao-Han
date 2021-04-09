ab72e0e04200c794729ab7d2117df65234705bb8
Fanghao-Han <han1652@outlook.com>
2021-04-09 03:49:08 -0600

    dht

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,12 +7,12 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 #'COMMIT VERSION NUMBER' 
-AESD_ASSIGNMENTS_VERSION = 52d86242ec0bf07fab7d8186d6c42d93d561e4eb
+AESD_ASSIGNMENTS_VERSION = 08d2f2429847b77f4b0705140f0a92d50525d577
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 #'GITHUB REPOSITORY LINK'
-AESD_ASSIGNMENTS_SITE =git@github.com:cu-ecen-5013/assignment-3-Fanghao-Han.git
+AESD_ASSIGNMENTS_SITE =git@github.com:Fanghao-Han/ecen-5013-final-project-application.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
@@ -25,17 +25,19 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	#$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/bin/conf/
 	#$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/bin/conf/
 	#$(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/root/
-#
+
 	#$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/* $(TARGET_DIR)/bin
-#
+
 	#$(INSTALL) -d 0755 $(@D)/server/ $(TARGET_DIR)/bin/server
 	#$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
 	#
 	#ln -s  $(TARGET_DIR)/usr/bin/simpleserver $(TARGET_DIR)/etc/init.d/S99simpleserver
 	#$(INSTALL) -m 0755 $(@D)/server/Smydaemon  $(TARGET_DIR)/etc/init.d/S99simpleserver
-#
+
 	#$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesd_load_qemu $(TARGET_DIR)/usr/bin/
 	#$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesd_unload_qemu $(TARGET_DIR)/usr/bin/
+
+	$(INSTALL) -m 0755 $(@D)/dht $(TARGET_DIR)/usr/bin/
 endef
 
 $(eval $(generic-package))
